2d7f47b469ffaf869a0ad490dfa495431726d0a8
Md. Suhag Mia <mdshohagmia53200@gmail.com>
2025-11-18 22:21:26 +0600

    mess responsive

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,12 +1,13 @@
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import React, { useEffect, useRef, useState } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import Container from './Container'
-import { FaCartPlus, FaStar, FaStarHalfAlt } from 'react-icons/fa'
-import { CiHeart, CiStar } from 'react-icons/ci'
+import { FaCartPlus, FaHeart, FaStar, FaStarHalfAlt } from 'react-icons/fa'
+import { CiHeart, CiStar, CiZoomIn } from 'react-icons/ci'
 import { useDispatch, useSelector } from 'react-redux'
 import { addToCart, addToWishlist } from './slice/productSlice'
 import { toast } from 'react-toastify'
+import { RiCloseLargeFill } from 'react-icons/ri'
 
 const ProductDetails = () => {
   let productId = useParams()
@@ -21,21 +22,21 @@ const ProductDetails = () => {
     getProductId()
   },[productId])
 
-  // let [relatedProducts, setRelatedProducts] = useState([])
-  // let categoryCus = singleProduct.category
-  // let getRelatedProducts = ()=>{
-  //   if(categoryCus){
-  //     axios.get(`https://fakestoreapi.com/products/category/${singleProduct.category}`).then((res)=>{
-  //       let filtered = res.data.filter(item => item.id !== singleProduct.id);
-  //       setRelatedProducts(filtered);
-  //     })
-  //   }
-  // }
-  // useEffect(()=>{
-  //   getRelatedProducts()
-  // },[relatedProducts])
+  let [relatedProducts, setRelatedProducts] = useState([])
+  let categoryCus = singleProduct.category
+  let getRelatedProducts = ()=>{
+    if(categoryCus){
+      axios.get(`https://fakestoreapi.com/products/category/${singleProduct.category}`).then((res)=>{
+        let filtered = res.data.filter(item => item.id !== singleProduct.id);
+        setRelatedProducts(filtered);
+      })
+    }
+  }
+  useEffect(()=>{
+    getRelatedProducts()
+  },[singleProduct])
 
-  // console.log(relatedProducts)
+  console.log(relatedProducts)
 
   let clientRatting = Array.from({length:5}, (_, index)=>{
     let number = index + 0.5
@@ -69,6 +70,22 @@ const ProductDetails = () => {
     toast.success("Added to Wishlist Successfully!")
   }
 }
+
+  let [zoomIn, setZoomIn] = useState(false)
+  let handleZoomIn = (item) => {
+    setZoomIn(item.image)
+  }
+
+  let zoomRef = useRef()
+  useEffect(() => {
+    let handleClickOutsite = (e) => {
+      if (zoomIn && !zoomRef.current.contains(e.target)) {
+        setZoomIn(false)
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutsite)
+    return () => document.removeEventListener("mousedown", handleClickOutsite)
+  }, [zoomIn])
 
   return (
     <section className='py-16 bg-gray-50'>
@@ -114,9 +131,56 @@ const ProductDetails = () => {
             </div>
           </div>
           <div>
-            <h2 className='text-xl font-bold font-jose text-orange-700 pt-12'>Related proudcts</h2>
+            <h2 className='text-xl font-bold font-jose text-orange-700 pt-12 pb-6'>Related proudcts</h2>
+            {relatedProducts.length > 0 ? <div className='grid grid-cols-2 lg:grid-cols-3 gap-x-4'>
+              {relatedProducts.map((item) => (
+                <div className='mb-4 shadow'>
+                  <div className='relative group overflow-x-hidden'>
+                    <Link to={`/products/${item.id}`}>
+                      <img src={item.image} alt={item.title} className=' bg-gray-300 py-6 object-contain w-full h-52 px-4' />
+                    </Link>
+                    <div className='absolute top-4 -left-14 group-hover:left-2 opacity-0 group-hover:opacity-100 py-2 transition-all duration-500 ease-in-out'>
+                      <div className='pb-4' onClick={() => handleCart(item)}>
+                        <div className='cursor-pointer text-[#767676] text-[20px] font-dms font-medium hover:text-[#ee00ff] pl-1'>
+                          <FaCartPlus />
+                        </div>
+                      </div>
+                      <div className='pb-4'>
+                        <div className='cursor-pointer text-[#767676] text-[20px] font-dms font-medium hover:text-[#ee00ff] pl-1'>
+                          <FaHeart onClick={() => handleWish(item)} />
+                        </div>
+                      </div>
+                      <div className='pb-4'>
+                        <div onClick={() => handleZoomIn(item)} className='cursor-pointer text-[#767676] text-[16px] font-dms font-medium hover:text-[#ee00ff]'>
+                          <CiZoomIn className='text-3xl' />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='py-4 px-3'>
+                    <Link to={`/products/${item.id}`}>
+                      <h2 className='text-[14px] font-bold font-jose text-violet-950 hover:underline line-clamp-2'>{item.title}</h2>
+                    </Link>
+                    <p className='text-[14px] font-bold font-jose text-violet-950 hover:underline pt-2'>{item.price}</p>
+                  </div>
+                </div>
+              ))}
+            </div> : <div><h2 className='text-2xl font-bold font-jose text-pink-500 text-center py-16'>No Related Products</h2></div> }
           </div>
         </div>
+        {zoomIn && (
+          <div ref={zoomRef} className='fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-[99999] bg-gray-100 px-20 rounded-2xl shadow-2xl border border-[#0000004b]'>
+            <div className='relative'>
+              <img src={zoomIn} className='max-w-7xl max-h-[calc(100vh-60px)] py-6' alt="" />
+              <div className='absolute top-4 -right-16'>
+                <RiCloseLargeFill
+                  onClick={() => setZoomIn(false)}
+                  className='text-5xl bg-red-500 text-white p-2 rounded-full cursor-pointer hover:bg-red-700 duration-300 font-extrabold'
+                />
+              </div>
+            </div>
+          </div>
+        )}
       </Container>
     </section>
   )
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -59,103 +59,195 @@ const Cart = () => {
       <Container>
         <div className='relative'>
           {rdata.length > 0 && (
-            <div className='py-16 flex items-center justify-center gap-x-2'>
-              <FaCartPlus className='text-4xl text-violet-950' />
-              <h2 className='text-center text-3xl font-jose font-extrabold text-violet-950'>Your Shopping Cart ({rdata.length})</h2>
+            <div className='py-8 sm:py-16 flex items-center justify-start lg:justify-center gap-x-2'>
+              <FaCartPlus className='text-xl sm:text-4xl text-violet-950' />
+              <h2 className='text-center text-md sm:text-3xl font-jose font-extrabold text-violet-950'>Your Shopping Cart ({rdata.length})</h2>
             </div>
           )}
           {rdata.length > 0 ? (
             <div>
-              <div className="overflow-x-auto rounded-lg shadow-md">
-                <table className="min-w-full bg-white border border-gray-200">
+              <div className='hidden md:block'>
+                <div className="overflow-x-auto rounded-lg shadow-md">
+                  <table className="min-w-full bg-white border border-gray-200">
+                    <thead className="bg-gray-100">
+                      <tr className="text-left text-indigo-950 font-bold text-lg">
+                        <th className="px-6 py-4">Product</th>
+                        <th className="px-6 py-4">Price</th>
+                        <th className="px-6 py-4">Remove</th>
+                        <th className="px-6 py-4">Total</th>
+                        <th className="px-6 py-4">Quantity</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {rdata.map((item, index) => (
+                        <tr key={item.id} className="border-t-2 border-[#0000001d] hover:bg-gray-50 transition">
+                          <td className="px-6 py-4 flex items-center gap-4">
+                            <img src={item.image} alt={item.title} className="h-20 w-20 object-contain" />
+                            <tr>
+                              <h2 className="font-semibold font-jose text-indigo-950 text-[14px] lg:text-[16px]">{item.title}</h2>
+                              <h3 className="font-medium font-lat text-gray-600 text-[12px] lg:text-[16px]">{item.category}</h3>
+                            </tr>
+                          </td>
+                          <td className="px-6 py-4 text-gray-700 font-semibold">${item.price}</td>
+                          <td className="px-6 py-4">
+                            <button
+                              onClick={() => dispatch(productRemove(index))}
+                              className="text-red-500 hover:text-red-400 font-semibold cursor-pointer">
+                              Remove
+                            </button>
+                          </td>
+                          <td className="px-6 py-4 font-semibold text-gray-800">
+                            ${(item.price * item.qun).toFixed(2)}
+                          </td>
+                          <td className="px-6 py-4">
+                            <div className="flex items-center gap-3">
+                              <button
+                                onClick={() => dispatch(decrement(item))}
+                                className={`px-2 py-1 bg-gray-200 rounded   ${item.qun <= 1 ? "opacity-50 cursor-not-allowed" : "opacity-100 hover:bg-gray-300 cursor-pointer"}`}>
+                                -
+                              </button>
+                              <span className="font-medium">{item.qun}</span>
+                              <button
+                                onClick={() => dispatch(increment(item))}
+                                className="px-2 py-1 bg-gray-200 rounded hover:bg-gray-300 cursor-pointer">
+                                +
+                              </button>
+                            </div>
+                          </td>
+                          <div className="absolute top-8 sm:top-16 right-0" onClick={() => dispatch(productRemoveAll(item))}>
+                            <h2 className='text-end text-[10px] sm:text-[18px] font-jose font-bold text-white bg-red-500 px-2 sm:px-4 py-1 sm:py-2 rounded-[5px] inline-block cursor-pointer hover:bg-red-400'>Delete All</h2>
+                          </div>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+                <table className="w-full max-w-xl ml-auto mt-10 border border-gray-300 rounded-lg shadow-md bg-white">
                   <thead className="bg-gray-100">
-                    <tr className="text-left text-indigo-950 font-bold text-lg">
-                      <th className="px-6 py-4">Product</th>
-                      <th className="px-6 py-4">Price</th>
-                      <th className="px-6 py-4">Remove</th>
-                      <th className="px-6 py-4">Total</th>
-                      <th className="px-6 py-4">Quantity</th>
+                    <tr>
+                      <th className="text-left px-6 py-4 text-indigo-950 font-bold text-lg border-b border-gray-300">Summary</th>
+                      <th className="text-right px-6 py-4 text-indigo-950 font-bold text-lg border-b border-gray-300">Amount</th>
                     </tr>
                   </thead>
                   <tbody>
-                    {rdata.map((item, index) => (
-                      <tr key={item.id} className="border-t-2 border-[#0000001d] hover:bg-gray-50 transition">
-                        <td className="px-6 py-4 flex items-center gap-4">
-                          <img src={item.image} alt={item.title} className="h-20 w-20 object-contain" />
-                          <tr>
-                            <h2 className="font-semibold font-jose text-indigo-950">{item.title}</h2>
-                            <h3 className="font-medium font-lat text-gray-600">{item.category}</h3>
-                          </tr>
-                        </td>
-                        <td className="px-6 py-4 text-gray-700 font-semibold">${item.price}</td>
-                        <td className="px-6 py-4">
-                          <button
-                            onClick={() => dispatch(productRemove(index))}
-                            className="text-red-500 hover:text-red-400 font-semibold cursor-pointer">
-                            Remove
-                          </button>
-                        </td>
-                        <td className="px-6 py-4 font-semibold text-gray-800">
-                          ${(item.price * item.qun).toFixed(2)}
-                        </td>
-                        <td className="px-6 py-4">
-                          <div className="flex items-center gap-3">
-                            <button
-                              onClick={() => dispatch(decrement(item))}
-                              className={`px-2 py-1 bg-gray-200 rounded   ${item.qun <= 1 ? "opacity-50 cursor-not-allowed" : "opacity-100 hover:bg-gray-300 cursor-pointer"}`}>
-                              -
-                            </button>
-                            <span className="font-medium">{item.qun}</span>
-                            <button
-                              onClick={() => dispatch(increment(item))}
-                              className="px-2 py-1 bg-gray-200 rounded hover:bg-gray-300 cursor-pointer">
-                              +
-                            </button>
-                          </div>
-                        </td>
-                        <div className="absolute top-16 right-0" onClick={() => dispatch(productRemoveAll(item))}>
-                          <h2 className='text-end text-[18px] font-jose font-bold text-white bg-red-500 px-4 py-2 rounded-[5px] inline-block cursor-pointer hover:bg-red-400'>Delete All</h2>
-                        </div>
-                      </tr>
-                    ))}
+                    <tr>
+                      <td className="px-6 py-4 text-gray-700 font-medium border-b border-gray-200">Subtotal</td>
+                      <td className="px-6 py-4 text-right text-gray-800 font-semibold border-b border-gray-200">${totalPrice.toFixed(2)}</td>
+                    </tr>
+                    <tr>
+                      <td className="px-6 py-4 text-gray-700 font-medium border-b border-gray-200">Total Quantity</td>
+                      <td className="px-6 py-4 text-right text-gray-800 font-semibold border-b border-gray-200">{totalQuantity}</td>
+                    </tr>
+                    <tr>
+                      <td className="px-6 py-4 text-gray-700 font-medium border-b border-gray-200">Shipping</td>
+                      <td className="px-6 py-4 text-right text-gray-800 font-semibold border-b border-gray-200">$15.00</td>
+                    </tr>
+                    <tr>
+                      <td className="px-6 py-4 text-indigo-950 font-bold border-b border-gray-300">Total</td>
+                      <td className="px-6 py-4 text-right text-indigo-950 font-bold border-b border-gray-300">
+                        ${(totalPrice + 15).toFixed(2)}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td colSpan="2" className="px-6 py-4 text-center">
+                        <Link to={"/process"} className="w-full bg-blue-600 text-white py-3 rounded hover:bg-blue-500 font-semibold inline-block hover:scale-102 transition duration-500 ease-in-out">
+                          Process to Checkout
+                        </Link>
+                      </td>
+                    </tr>
                   </tbody>
                 </table>
               </div>
-              <table className="w-full max-w-xl ml-auto mt-10 border border-gray-300 rounded-lg shadow-md bg-white">
-                <thead className="bg-gray-100">
-                  <tr>
-                    <th className="text-left px-6 py-4 text-indigo-950 font-bold text-lg border-b border-gray-300">Summary</th>
-                    <th className="text-right px-6 py-4 text-indigo-950 font-bold text-lg border-b border-gray-300">Amount</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td className="px-6 py-4 text-gray-700 font-medium border-b border-gray-200">Subtotal</td>
-                    <td className="px-6 py-4 text-right text-gray-800 font-semibold border-b border-gray-200">${totalPrice.toFixed(2)}</td>
-                  </tr>
-                  <tr>
-                    <td className="px-6 py-4 text-gray-700 font-medium border-b border-gray-200">Total Quantity</td>
-                    <td className="px-6 py-4 text-right text-gray-800 font-semibold border-b border-gray-200">{totalQuantity}</td>
-                  </tr>
-                  <tr>
-                    <td className="px-6 py-4 text-gray-700 font-medium border-b border-gray-200">Shipping</td>
-                    <td className="px-6 py-4 text-right text-gray-800 font-semibold border-b border-gray-200">$15.00</td>
-                  </tr>
-                  <tr>
-                    <td className="px-6 py-4 text-indigo-950 font-bold border-b border-gray-300">Total</td>
-                    <td className="px-6 py-4 text-right text-indigo-950 font-bold border-b border-gray-300">
-                      ${(totalPrice + 15).toFixed(2)}
-                    </td>
-                  </tr>
-                  <tr>
-                    <td colSpan="2" className="px-6 py-4 text-center">
-                      <Link to={"/process"} className="w-full bg-blue-600 text-white py-3 rounded hover:bg-blue-500 font-semibold inline-block hover:scale-102 transition duration-500 ease-in-out">
-                        Process to Checkout
-                      </Link>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+              <div className='md:hidden'>
+                <div className="overflow-x-auto rounded-lg shadow-md">
+                  <table className="min-w-full bg-white border-t-3 border-gray-200">
+                    <tbody>
+                      {rdata.map((item, index) => (
+                        <tr key={item.id} className="border-t-2 border-[#0000001d] hover:bg-gray-50 transition">
+                          <td className="px-6 py-4 flex items-center gap-4">
+                            <img src={item.image} alt={item.title} className="h-20 w-20 object-contain" />
+                            <tr>
+                              <h2 className="font-semibold font-jose text-indigo-950">{item.title}</h2>
+                              <h3 className="font-medium font-lat text-gray-600">{item.category}</h3>
+                            </tr>
+                          </td>
+                          <tr className='flex items-center justify-between px-6'>
+                            <div>
+                              <p className="text-gray-700 font-semibold">${item.price}</p>
+                            </div>
+                            <div>
+                              <button
+                                onClick={() => dispatch(productRemove(index))}
+                                className="text-red-500 hover:text-red-400 font-semibold cursor-pointer">
+                                Remove
+                              </button>
+                            </div>
+                            <div className="font-semibold text-gray-800">
+                              ${(item.price * item.qun).toFixed(2)}
+                            </div>
+                          </tr>
+                          <tr>
+                            <td className="px-6 py-4">
+                              <div className="flex items-center gap-3">
+                                Qunatitu:
+                                <button
+                                  onClick={() => dispatch(decrement(item))}
+                                  className={`px-2 py-1 bg-gray-200 rounded   ${item.qun <= 1 ? "opacity-50 cursor-not-allowed" : "opacity-100 hover:bg-gray-300 cursor-pointer"}`}>
+                                  -
+                                </button>
+                                <span className="font-medium">{item.qun}</span>
+                                <button
+                                  onClick={() => dispatch(increment(item))}
+                                  className="px-2 py-1 bg-gray-200 rounded hover:bg-gray-300 cursor-pointer">
+                                  +
+                                </button>
+                              </div>
+                            </td>
+                          </tr>
+                          <div className="absolute top-8 sm:top-16 right-0" onClick={() => dispatch(productRemoveAll(item))}>
+                            <h2 className='text-end text-[10px] sm:text-[18px] font-jose font-bold text-white bg-red-500 px-2 sm:px-4 py-1 sm:py-2 rounded-[5px] inline-block cursor-pointer hover:bg-red-400'>Delete All</h2>
+                          </div>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+                <table className="w-full max-w-xl ml-auto mt-10 border border-gray-300 rounded-lg shadow-md bg-white">
+                  <thead className="bg-gray-100">
+                    <tr>
+                      <th className="text-left px-6 py-4 text-indigo-950 font-bold text-lg border-b border-gray-300">Summary</th>
+                      <th className="text-right px-6 py-4 text-indigo-950 font-bold text-lg border-b border-gray-300">Amount</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className="px-6 py-4 text-gray-700 font-medium border-b border-gray-200">Subtotal</td>
+                      <td className="px-6 py-4 text-right text-gray-800 font-semibold border-b border-gray-200">${totalPrice.toFixed(2)}</td>
+                    </tr>
+                    <tr>
+                      <td className="px-6 py-4 text-gray-700 font-medium border-b border-gray-200">Total Quantity</td>
+                      <td className="px-6 py-4 text-right text-gray-800 font-semibold border-b border-gray-200">{totalQuantity}</td>
+                    </tr>
+                    <tr>
+                      <td className="px-6 py-4 text-gray-700 font-medium border-b border-gray-200">Shipping</td>
+                      <td className="px-6 py-4 text-right text-gray-800 font-semibold border-b border-gray-200">$15.00</td>
+                    </tr>
+                    <tr>
+                      <td className="px-6 py-4 text-indigo-950 font-bold border-b border-gray-300">Total</td>
+                      <td className="px-6 py-4 text-right text-indigo-950 font-bold border-b border-gray-300">
+                        ${(totalPrice + 15).toFixed(2)}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td colSpan="2" className="px-6 py-4 text-center">
+                        <Link to={"/process"} className="w-full bg-blue-600 text-white py-3 rounded hover:bg-blue-500 font-semibold inline-block hover:scale-102 transition duration-500 ease-in-out">
+                          Process to Checkout
+                        </Link>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           ) : (
             <div className="py-12">
